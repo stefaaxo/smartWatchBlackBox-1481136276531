3c8ae8b9307ebe7877a4ee9e33e085c2b8520d2c
stefaaxo <stefy@us.ibm.com>
2016-12-07 14:25:45 -0500

    Update SimpleServlet.java
    stefy test

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -20,8 +20,32 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        // response.setContentType("text/html");
+        // response.getWriter().print("Hello World!");
+        
+         try{
+         // just the name of the parameter in the URL    
+	   	 String fileUrl = request.getParameter("fileUrl");
+         URL url = new URL(fileUrl);
+             
+	     response.getWriter().println("my file is: " + url.getName());
+             
+         // Create a temp file     
+	     File tmp = File.createTempFile("myTmp", ".csv");
+	     tmp.deleteOnExit();     	 
+	     IOUtils.copy(url.openStream(), new FileOutputStream(tmp));
+	 
+	     
+	 
+	     response.setContentType("application/json");
+	     response.getWriter().write("Stefy");
+
+	   }catch(Exception e){
+	     response.setContentType("text/html");	    	    
+        response.getWriter().println("Hello no good parameter!");
+        e.printStackTrace(response.getWriter());
+        
+	   }
     }
 
 }
