852cece2697f57389d8af5777338c69cf1c961f7
stefaaxo <stefy@us.ibm.com>
2016-12-14 20:28:01 -0500

    Create HeartRateThreshold.java
    test thresholds

---
+++ src/main/java/ibm/smartWatch/heartRate/HeartRateThreshold.java
@@ -0,0 +1,6 @@
+package ibm.smartWatch.heartRate;
+
+public interface HeartRateThreshold {
+	static Integer minHeartRate = new Integer(40);  // falling asleep
+	static Integer maxHeartRate = new Integer(120); // adrenaline boost 
+}
